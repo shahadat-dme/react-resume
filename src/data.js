bd6fa989d3185566711cdd01103389de4d41473c
Shahadat <shahadat2cp17@gmail.com>
2021-12-10 22:44:50 +0600

    add

---
+++ src/data.js
@@ -0,0 +1,38 @@
+import image1 from './img/image1.PNG'
+import image2 from './img/image2.PNG'
+import image3 from './img/image3.PNG'
+import image4 from './img/image4.PNG'
+import image5 from './img/image5.PNG'
+import image6 from './img/image6.PNG'
+export const products = [
+  {
+    id: 1,
+    img: image1,
+    link: "https://organic-food.vercel.app/",
+  },
+  {
+    id: 2,
+    img: image2,
+    link: "https://react-real-estate.vercel.app/",
+  },
+  {
+    id: 3,
+    img: image3,
+    link: "https://react-gym-web.vercel.app/",
+  },
+  {
+    id: 4,
+    img: image4,
+    link: "https://react-landing-page-bd.vercel.app/",
+  },
+  {
+    id: 5,
+    img: image5,
+    link:"https://react-single-page.vercel.app/",
+  },
+  {
+    id: 6,
+    img: image6,
+    link: "https://restaurants-web-nu.vercel.app/",
+  },
+];
